53da2ce07fc122e7fdf68c4ca90710bb3707d2b2
liamzety <58632095+liamzety@users.noreply.github.com>
2021-05-02 17:03:22 +0300

    Updates

---
+++ precache-manifest.fa5eb74ac5175100d256f22ea60c7dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c0f0efa1c8140b3ebdc07db20d838c",
+    "url": "/edea-challenge/index.html"
+  },
+  {
+    "revision": "8954f4d50646864f9b01",
+    "url": "/edea-challenge/static/css/main.100ef3bd.chunk.css"
+  },
+  {
+    "revision": "ad80bf2071bfd7f41b49",
+    "url": "/edea-challenge/static/js/2.939cbc6f.chunk.js"
+  },
+  {
+    "revision": "17100c8c3f9fc3b14ffb7f5a0710499d",
+    "url": "/edea-challenge/static/js/2.939cbc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8954f4d50646864f9b01",
+    "url": "/edea-challenge/static/js/main.207a2fc0.chunk.js"
+  },
+  {
+    "revision": "3b437ac78e148d19a906",
+    "url": "/edea-challenge/static/js/runtime-main.8f63bf5a.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/edea-challenge/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
